7d7a5f1279c76bd6d8b1c45c331dedd8c70ac3e4
mdsiamss10 <mdsiamss10@gmail.com>
2023-03-17 10:05:37 +0600

    Testing roleAction page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import Message from "@/components/Message";
 import Skeleton from "@/components/Skeleton";
+import { db } from "@/firebase.config";
+import { deleteDoc, doc } from "firebase/firestore";
 import { useContext } from "react";
-import { PostsContext } from "./_app";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { toast } from "react-toastify";
+import { PostsContext, UserContext } from "./_app";
 
 const Home = () => {
   const { posts } = useContext(PostsContext);
+  const { user } = useContext(UserContext);
+
+  // Delete a post
+  const deletePost = (id) => {
+    if (confirm("Are you sure you want to delete this post?")) {
+      const docRef = doc(db, "posts", id);
+      deleteDoc(docRef)
+        .then(() => {
+          toast.success("Post deleted successfully");
+        })
+        .catch((err) => {
+          toast.error("Error deleting post");
+        });
+    }
+  };
+
   return (
     <>
       <div className="my-12 text-lg font-medium">
@@ -14,7 +34,23 @@ const Home = () => {
         {posts.length ? (
           posts.map((post) => (
             <>
-              <Message {...post} />
+              {post.approved && user?.email === "ohiduzzamansiam@gmail.com" ? (
+                <Message key={post?.id} {...post}>
+                  <div className="flex items-center gap-3">
+                    <div
+                      onClick={() => {
+                        deletePost(post.id);
+                      }}
+                      className="flex items-center gap-1 font-bold text-red-600 cursor-pointer select-none"
+                    >
+                      <AiTwotoneDelete />
+                      <span>delete</span>
+                    </div>
+                  </div>
+                </Message>
+              ) : (
+                <Message {...post} />
+              )}
             </>
           ))
         ) : (
